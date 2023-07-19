5f8127046217d474b598a9d35b2fa3d294ec9b7a
OumniaBASSI <oumniabassi@gmail.com>
2023-07-18 18:07:58 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_sign - chek input if its positive, negative or zero
+ * print_sign - check input if its positive, negative or zero
  * @n: variable to determine if input is greater than, less than or is zero
  * Retiurn: 0 if succesful
  */
@@ -16,7 +16,7 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	if (n <0)
+	if (n < 0)
 	{
 		_putchar('-');
 	}
