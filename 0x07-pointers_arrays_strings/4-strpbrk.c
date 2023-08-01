ae08aa2d59812500deb31706a97913efac94960c
OumniaBASSI <oumniabassi@gmail.com>
2023-07-31 17:07:49 -0700

    do hard things

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,23 +1,27 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * *_strpbrk - Entry function
- * @s: char
- * @accept: char
- * Return: s or NULL (Success)
+ * *_strpbrk - searches a string for any of a set of bytes
+ * @s: string to search
+ * @accept: string containing the bytes to look for
+ *
+ * Return: pointer to the byte in s that matches one of the bytes in accept or NULL if no such bytes is found
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i;
+	int i, j;
 
-	while (*s != '\0')
+	for (i = 0; *s != '\0'; i++)
 	{
-		for (i = 0; accept[i] != '0'; i++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if(*s == accept[i])
+			if (*s == accept[j])
+			{
 				return (s);
+			}
 		}
 		s++;
 	}
+
 	return (NULL);
 }
