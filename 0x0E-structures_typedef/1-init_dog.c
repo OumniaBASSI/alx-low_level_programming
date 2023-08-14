e68e2c2670b0c17e67e083bc5edcb0fa6e311202
OumniaBASSI <oumniabassi@gmail.com>
2023-08-14 15:55:41 -0700

    do hard things

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - Entry function
+ * @d: struct dog
+ * @name: char
+ * @age: float
+ * @owner: char
+ */
+void init_dog(struct dog *d, chr *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
