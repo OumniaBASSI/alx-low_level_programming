61918e50f54c42e048ed04f70afa296c9a67f2fc
OumniaBASSI <oumniabassi@gmail.com>
2023-08-17 18:52:27 -0700

    DO HARD THINGS

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,14 +8,17 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list list;
-	unsigned int i, sum;
+	unsigned int i;
+	int sum = 0;
 
-	sum = 0;
+	
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(list, int);
 	}
+
 	va_end(list);
+
 	return (sum);
 }
